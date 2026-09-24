4fb7371ea65f379bf28c5fda0f8a81578b7577dd
Sina <sinagessner.wuppertal@gmail.com>
2026-09-24 03:35:07 +0200

    Expose website publish action to editor

--- editor/preload.cjs
+++ editor/preload.cjs
@@ -6,6 +6,7 @@ contextBridge.exposeInMainWorld('ccWebsiteEditor', {
   listPages: () => ipcRenderer.invoke('editor:list-pages'),
   loadPage: page => ipcRenderer.invoke('editor:load-page', page),
   savePage: payload => ipcRenderer.invoke('editor:save-page', payload),
+  publishSite: () => ipcRenderer.invoke('editor:publish-site'),
   importImages: () => ipcRenderer.invoke('editor:import-images'),
   openPreview: page => ipcRenderer.invoke('editor:open-preview', page),
 });
